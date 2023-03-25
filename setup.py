a76fc75712fa7f9d136f9833b9a5a224d165e045
Ian Williamson <ian.williamson@utexas.edu>
2023-03-25 08:13:06 -0700

    Single source version number

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 """Setup imageruler."""
+import codecs
+import os.path
 import setuptools
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError('Unable to find version string.')
+
 
 setuptools.setup(
     name='imageruler',
-    version='1.0',
+    version=get_version('imageruler/__init__.py'),
     license='MIT',
     author='',
     author_email='',
