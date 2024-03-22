bb162bb5e75b5e7db3d7eedd8178cac36c39c18a
Martin Schubert <mfschubert@gmail.com>
2024-03-21 17:38:20 -0700

    Type annotation

--- imageruler/__main__.py
+++ imageruler/__main__.py
@@ -10,7 +10,7 @@ import numpy as onp
 import imageruler
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(
         description=(
             "A python package for estimating minimum lengthscales in binary" " images"
@@ -22,8 +22,8 @@ def main():
     args = parser.parse_args()
 
     design = onp.loadtxt(args.file, delimiter=",")
-    design = design > 0.5
-    solid_mls, void_mls = imageruler.minimum_length_scale(design)
+    binarized_design = design > 0.5
+    solid_mls, void_mls = imageruler.minimum_length_scale(binarized_design)
     print("Minimum lengthscales of solid and void regions: ", solid_mls, void_mls)
 
 
